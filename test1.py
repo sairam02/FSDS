31849952c3226c49f65bb47516dfac4cc30f45b8
sairam02 <93117382+sairam02@users.noreply.github.com>
2022-01-22 15:47:36 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+
+
+def test(a,b):
+    """ This is my customised function for addition """
+    return a + b
+    
+def test1(b,c):
+    return b*c
+    
+def test3(b,n):
+    return b/n
+    
+def test4():
+    print("This is my code ")
