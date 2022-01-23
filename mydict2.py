ec48118def678124ab0d9d306fe2ca883da40f55
sairam02 <93117382+sairam02@users.noreply.github.com>
2022-01-23 13:05:47 +0530

    Add files via upload

---
+++ mydict2.py
@@ -0,0 +1,59 @@
+class dict_parsing:
+    
+    def __init__(self,a):
+        self.a = a
+        
+    def getkeys(self):
+        if self.notdict():
+            return list(self.a.keys())
+        
+    def getvalues(self):
+        if self.notdict():
+            return list(self.a.values())
+        
+        
+    def notdict(self):
+        if type(self.a) != dict:
+            raise Exception(self.a, " Not a Dictionary ")
+        return 1
+    
+    def userinput(self):
+        self.a = eval(input())
+        print(self.a, type(self.a))
+        print(self.getkeys())
+        print(self.getvalues())
+        
+    def insertion(self,k,v):
+        self.a[k] = v
+        
+
+class dict_parsing2:
+    
+    def __init__(self,a):
+        self.a = a
+        
+    def getkeys(self):
+        if self.notdict():
+            return list(self.a.keys())
+        
+    def getvalues(self):
+        if self.notdict():
+            return list(self.a.values())
+        
+        
+    def notdict(self):
+        if type(self.a) != dict:
+            raise Exception(self.a, " Not a Dictionary ")
+        return 1
+    
+    def userinput(self):
+        self.a = eval(input())
+        print(self.a, type(self.a))
+        print(self.getkeys())
+        print(self.getvalues())
+        
+    def insertion(self,k,v):
+        self.a[k] = v
+        
+        
+    
